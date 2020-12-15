67294490c365a267c0d6169c63fae61aedf62d87
Julien Dehos <dehos@univ-littoral.fr>
2020-12-15 22:51:28 +0100

    cuda

--- 20201030-nixpkgs-pytorch/default.nix
+++ 20201030-nixpkgs-pytorch/default.nix
@@ -1,10 +1,12 @@
 let
   channel = fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/0b875aa.tar.gz";
-    sha256 = "sha256:1pr56w356s67hnmjjarbzpmv1hwb0rd9nq6rcnxb3dxss5r3i5hr";
+    url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
+    #url = "https://github.com/NixOS/nixpkgs/archive/0b875aa.tar.gz";
+    #sha256 = "sha256:1pr56w356s67hnmjjarbzpmv1hwb0rd9nq6rcnxb3dxss5r3i5hr";
   };
 
-  pkgs = import channel {};
+  pkgs = import ../../nixpkgs {};
+  #pkgs = import channel {};
 
 in with pkgs; stdenv.mkDerivation {
   name = "myapp";
--- 20201030-nixpkgs-pytorch/main.cpp
+++ 20201030-nixpkgs-pytorch/main.cpp
@@ -2,9 +2,20 @@
 #include <iostream>
 
 int main() {
-    torch::Device device(torch::kCPU);
+
+    torch::DeviceType device_type;
+    if (torch::cuda::is_available()) {
+        std::cout << "CUDA available! Training on GPU." << std::endl;
+        device_type = torch::kCUDA;
+    } else {
+        std::cout << "Training on CPU." << std::endl;
+        device_type = torch::kCPU;
+    }
+    torch::Device device(device_type);
+
     torch::Tensor tensor = torch::eye(3, device);
     std::cout << tensor << std::endl;
+
     return 0;
 }
 
